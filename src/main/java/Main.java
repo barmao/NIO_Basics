b9943695bc6cd94fbd45ac81100ead26f7aa67bf
barmao <bryanbarmao@gmail.com>
2021-06-24 22:47:42 +0300

    add EchoServer based on NIO

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,15 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 
 public class Main {
     public static  void main(String[] args) throws IOException {
         System.out.println("Hello World!"); // Display the string.
-        ServeNIO(3000);
+        serve(3000);
     }
 
     public static void ServeBlocking(int port) throws IOException{
@@ -119,6 +117,81 @@ public class Main {
 
                     }
                 }
+            }
+        }
+    }
+
+    public static void serve(int port) throws IOException {
+        System.out.println("Listening for connections on port " + port);
+        final AsynchronousServerSocketChannel serverChannel =
+                AsynchronousServerSocketChannel.open();
+        InetSocketAddress address = new InetSocketAddress(port);
+        serverChannel.bind(address); //#1Bind Server to port
+        final CountDownLatch latch = new CountDownLatch(1);
+        serverChannel.accept(null, new
+                CompletionHandler<AsynchronousSocketChannel, Object>() { //#2 Start to accept new Client connections. Once one is accepted the CompletionHandler will get called.
+                    @Override
+                    public void completed(final AsynchronousSocketChannel channel,
+                                          Object attachment) {
+                        serverChannel.accept(null, this); //#3 Again accept new Client connections
+                        ByteBuffer buffer = ByteBuffer.allocate(100);
+                        channel.read(buffer, buffer,
+                                new EchoCompletionHandler(channel));//#4 Trigger a read operation on the Channel, the given CompletionHandler will be notified once something was read
+                    }
+                    @Override
+                    public void failed(Throwable throwable, Object attachment) {
+                        try {
+                            serverChannel.close(); //#5 Close the socket on error
+                        } catch (IOException e) {
+// ingnore on close
+                        } finally {
+                            latch.countDown();
+                        }
+                    }
+                });
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    private static final class EchoCompletionHandler implements
+            CompletionHandler<Integer, ByteBuffer> {
+        private final AsynchronousSocketChannel channel;
+        EchoCompletionHandler(AsynchronousSocketChannel channel) {
+            this.channel = channel;
+        }
+        @Override
+        public void completed(Integer result, ByteBuffer buffer) {
+            buffer.flip();
+            channel.write(buffer, buffer, new CompletionHandler<Integer,
+                    ByteBuffer>() { //#6 Trigger a write operation on the Channel, the given CompletionHandler will be notified once  something was written
+                @Override
+                public void completed(Integer result, ByteBuffer buffer) {
+                    if (buffer.hasRemaining()) {
+                        channel.write(buffer, buffer, this); //#7 Trigger again a write operation if something is left in the ByteBuffer
+                    } else {
+                        buffer.compact();
+                        channel.read(buffer, buffer,
+                                EchoCompletionHandler.this); //#8 Trigger a read operation on the Channel, the given CompletionHandler will be notified once something was read
+                    }
+                }
+                @Override
+                public void failed(Throwable exc, ByteBuffer attachment) {
+                    try {
+                        channel.close();
+                    } catch (IOException e) {
+// ingnore on close
+                    }
+                }
+            });
+        }
+        @Override
+        public void failed(Throwable exc, ByteBuffer attachment) {
+            try {
+                channel.close();
+            } catch (IOException e) {
+// ingnore on close
             }
         }
     }
